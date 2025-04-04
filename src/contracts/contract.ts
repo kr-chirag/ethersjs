76087bd9acbe356ff2b727ffcb05e6c5878e34d9
kr-chirag <chirag.p@knackroot.com>
2025-04-04 17:52:36 +0530

    listening to contract events

--- src/contracts/contract.ts
+++ src/contracts/contract.ts
@@ -5,7 +5,7 @@ import CST_ABI from "./CST_ABI.json";
 async function main() {
     const provider = new ethers.JsonRpcProvider(RPC_INFURA_SEPOLIA);
     const signer = new Wallet(PRIVATE_KEY1, provider);
-    const contractAdress = "0x985102151bFa4b49FcEb489A09893b019d699d22";
+    const contractAdress = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238";
     const cstContract = new Contract(contractAdress, CST_ABI, signer);
 
     console.log("Symbol:", await cstContract.symbol());
@@ -13,6 +13,12 @@ async function main() {
     console.log("decimals:", decimals);
     const balance = await cstContract.balanceOf(ACCOUNT1);
     console.log("Balance:", formatUnits(balance, decimals));
+
+    // listening to contract events
+    cstContract.on("Transfer", (from, to, _amount, event) => {
+        const amount = ethers.formatUnits(_amount, 6);
+        console.log(`${from} => ${to}: ${amount}`);
+    });
 }
 
 main().catch(console.log);
